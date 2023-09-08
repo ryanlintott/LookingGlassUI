23e0abf3efecf2a10b37887db3b8510019f35321
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2023-09-07 21:44:06 -0400

    Removed commented out FirebladeMath dependency. Updated xcode settings Changed Twitter link to Mastodon

--- Package.swift
+++ Package.swift
@@ -16,8 +16,6 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-//        .package(name: "FirebladeMath", url: "https://github.com/fireblade-engine/math.git", from: "0.11.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
